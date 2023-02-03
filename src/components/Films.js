d22cb6698cc99c9fe31786bd1f753745298100b4
SerialPenguin <larsson.alexander@live.se>
2023-02-03 09:52:16 +0100

    FINAL

--- src/components/Films.js
+++ src/components/Films.js
@@ -4,34 +4,34 @@ function Films(props) {
   const [films, setFilms] = useState([]);
   const [selectedFilm, setSelectedFilm] = useState([]);
   const [currentPage] = useState();
-  const [inputValue, setInputValue] = useState('');
-  const [searchedItem, setSearchedItem] = useState(null);
-  const [noFound,setNoFound] = useState(false)
+  // const [inputValue, setInputValue] = useState('');
+  // const [searchedItem, setSearchedItem] = useState(null);
+  // const [noFound,setNoFound] = useState(false)
 
 
-  useEffect(() => {
-    const fetchSearch = async () => {
-      const response = await fetch(`https://swapi.dev/api/films/?search=${inputValue}`);
-      const data = await response.json();
-      if(data.results.length > 0){
-      setSearchedItem(data.results)}
-      else{setSearchedItem(null); setNoFound(true)}
-    };
+  // useEffect(() => {
+  //   const fetchSearch = async () => {
+  //     const response = await fetch(`https://swapi.dev/api/films/?search=${inputValue}`);
+  //     const data = await response.json();
+  //     if(data.results.length > 0){
+  //     setSearchedItem(data.results)}
+  //     else{setSearchedItem(null); setNoFound(true)}
+  //   };
 
-    if (inputValue !== '') {
-      // props.setState('search')
-      setNoFound(false)
-      fetchSearch();
-    }
-  }, [inputValue]);
+  //   if (inputValue !== '') {
+  //     props.setState('search')
+  //     setNoFound(false)
+  //     fetchSearch();
+  //   }
+  // }, [inputValue]);
 
-  const handleKeyDown = (event) => {
+  // const handleKeyDown = (event) => {
    
-    if (event.key === 'Enter') {
-      setInputValue(event.target.value);
+  //   if (event.key === 'Enter') {
+  //     setInputValue(event.target.value);
       
-    }
-  };
+  //   }
+  // };
 
   const fetchFilms = async () => {
     const res = await fetch("https://swapi.dev/api/films/");
@@ -62,14 +62,14 @@ function Films(props) {
     <div>
       {props.state === "films" && (
         <div>
-          <input placeholder="Search For Film..." type="text" onKeyDown={handleKeyDown} />
+          {/* <input placeholder="Search For Film..." type="text" onKeyDown={handleKeyDown} />
           {props.state === 'films' && searchedItem !== null ?  <div className="information"> <div><h3>Found: {JSON.stringify(searchedItem[0].title)}</h3> 
           <p>Episode nr: {searchedItem[0].episode_id}</p>
           <p>Director: {searchedItem[0].director}</p>
           <p>Producer: {searchedItem[0].producer}</p>
           <p>Release date: {searchedItem[0].release_date}</p>
          </div></div> : null} 
-          {noFound && <p>nothing found</p>}
+          {noFound && <p>nothing found</p>} */}
           {films.length > 0 && (
             <div>
               <div className="info-btns">
@@ -81,11 +81,11 @@ function Films(props) {
               </div>
             </div>
           )}
-        </div>
+       </div>
       )}
 
       {props.state === "films" && selectedFilm.title && (
-        <div className="information">
+        <div className="film-information">
         <div>
           <h3>{selectedFilm.title}</h3>
           <p>Episode nr: {selectedFilm.episode_id}</p>
